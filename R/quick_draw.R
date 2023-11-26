d2a8d43b3a08f7da8bb622fd45fdbb703c4a8d81
Bhargav Kantheti <bhargavkantheti5@gmail.com>
2023-11-25 20:56:06 -0500

    Add data categorization

--- R/quick_draw.R
+++ R/quick_draw.R
@@ -2,4 +2,18 @@
 
 # Automatically parse data, fill missing.
 
-quick_draw <- function(data) {}
+quick_draw <- function(df) {
+    df <- df
+    columns <- names(df)
+
+    for (col in columns) {
+        if (class(df[[col]]) == "numeric") {
+        } else if (class(df[[col]]) == "integer") {
+        } else if (class(df[[col]]) == "character") {
+        } else if (class(df[[col]]) == "logical") {
+        } else {
+            print("Data types not supported.")
+            return()
+        }
+    }
+}
